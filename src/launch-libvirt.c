27393283ac938dd80853e6bfdfd33b6fdd04215e
Richard W.M. Jones <rjones@redhat.com>
2016-04-05 15:02:20 +0100

    launch: libvirt: Disable virtio-rng in libvirt backend until we can use /dev/urandom.
    This updates commit 9423c16607259b30985c46d04db9958ec079aa42.
    
    (cherry picked from commit b2c845333f66d4d3135d3b6a8d4b992d6160bf0a)

--- src/launch-libvirt.c
+++ src/launch-libvirt.c
@@ -1247,6 +1247,12 @@ construct_libvirt_xml_devices (guestfs_h *g,
     }
 #endif
 
+#if 0
+    /* This is disabled.  Pulling random numbers from /dev/random
+     * causes the appliance to pause for long periods.  We should
+     * use /dev/urandom, but for bogus reasons libvirt prevents that.
+     * https://bugzilla.redhat.com/show_bug.cgi?id=1074464#c7
+     */
     /* Add a random number generator (backend for virtio-rng). */
     start_element ("rng") {
       attribute ("model", "virtio");
@@ -1260,6 +1266,7 @@ construct_libvirt_xml_devices (guestfs_h *g,
         //string ("/dev/urandom");
       } end_element ();
     } end_element ();
+#endif
 
     /* virtio-scsi controller. */
     start_element ("controller") {
